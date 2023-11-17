194464eecba5965e0723306c36727070229821df
Reda Ajdi <redaajdi6@gmail.com>
2023-11-17 20:49:10 +0100

    user registration

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\ListingController;
-use Illuminate\Support\Facades\Route;
 use App\Models\Listing;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -31,4 +32,9 @@ Route::put('/listings/{listing}', [ListingController::class,'update']);
 Route::delete('/listings/{listing}', [ListingController::class,'destroy']);
 
 Route::get('/listings/{listing}', [ListingController::class,'show']);
+
+// register user
+Route::get('/register', [UserController::class,'create']);
+
+Route::post('/users', [UserController::class,'store']);
 
